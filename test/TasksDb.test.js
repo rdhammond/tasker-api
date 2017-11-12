ddba69e0fda99cbfffdcda43ae36a8f7cc1ed615
R. Daniel Hammond <viztheman@theinternetemail.com>
2017-11-12 13:12:01 -0500

    TasksDb unit tests in place and working.

--- test/TasksDb.test.js
+++ test/TasksDb.test.js
@@ -11,6 +11,8 @@ const testEntries = [
 	{name: 'Test3', completed: 1}
 ];
 
+var db;
+
 async function loadEntries() {
 	const statement = await sqlite.prepare('INSERT INTO daily(lastUpdated, name, completed) VALUES(?, ?, ?);');
 
@@ -75,5 +77,169 @@ describe('TasksDb', function() {
 		});
 	});
 
-	// ** PICK UP HERE
+	describe('#update', function() {
+		var id;
+
+		beforeEach(loadEntries);
+		afterEach(clearEntries);
+
+		beforeEach(async function() {
+			db = new TasksDb();
+			await db.open({sqlite: DB_PATH});
+			id = (await sqlite.get('SELECT rowid FROM daily WHERE name = \'Test1\'')).rowid;
+		});
+
+		it('should return true if found', async function() {
+			const result = await db.update('daily', {
+				id,
+				lastUpdated: new Date().toISOString(),
+				name: 'Test Change',
+				completed: true
+			});
+			result.should.be.ok();
+		});
+
+		it('should return false if not found', async function() {
+			const result = await db.update('daily', {
+				id: 'ajkldfjl9',
+				lastUpdated: new Date().toISOString(),
+				name: 'Test Change',
+				completed: true
+			});
+			result.should.not.be.ok();
+		});
+
+		it('should update', async function() {
+			const now = new Date().toISOString();
+
+			await db.update('daily', {
+				id,
+				lastUpdated: now,
+				name: 'Test Change',
+				completed: false
+			});
+
+			const result = await sqlite.get('SELECT rowid, lastUpdated, name, completed FROM daily WHERE rowid = ?', id);
+			result.rowid.should.eql(id);
+			result.lastUpdated.should.eql(now);
+			result.name.should.eql('Test Change');
+		});
+
+		it('should convert completed true to 1', async function() {
+			await db.update('daily', {
+				id,
+				lastUpdated: new Date().toISOString(),
+				name: 'Test Change',
+				completed: true
+			});
+
+			const result = await sqlite.get('SELECT * FROM daily WHERE rowid = ?', id);
+			result.completed.should.eql(1);
+		});
+
+		it('should convert completed false to 0', async function() {
+			await db.update('daily', {
+				id,
+				lastUpdated: new Date().toISOString(),
+				name: 'Test Change',
+				completed: false
+			});
+
+			const result = await sqlite.get('SELECT * FROM daily WHERE rowid = ?', id);
+			result.completed.should.eql(0);
+		});
+	});
+
+	describe('#insert', function() {
+		afterEach(clearEntries);
+
+		beforeEach(async function() {
+			db = new TasksDb();
+			await db.open({sqlite: DB_PATH});
+		});
+
+		it('should return id on insert', async function() {
+			const result = await db.insert('daily', {
+				lastUpdated: new Date().toISOString(),
+				name: 'New Name',
+				completed: false
+			});
+
+			const realId = (await sqlite.get('SELECT rowid FROM daily')).rowid;
+			result.should.eql(realId);
+		});
+
+		it('should add new row', async function() {
+			const now = new Date().toISOString();
+
+			await db.insert('daily', {
+				lastUpdated: now,
+				name: 'New Name',
+				completed: false
+			});
+			
+			const result = await sqlite.get('SELECT lastUpdated, name FROM daily');
+			result.lastUpdated.should.eql(now);
+			result.name.should.eql('New Name');
+		});
+
+		it('should convert completed true to 1', async function() {
+			await db.insert('daily', {
+				lastUpdated: new Date().toISOString(),
+				name: 'New Name',
+				completed: true
+			});
+
+			const result = await sqlite.get('SELECT completed FROM daily');
+			result.completed.should.equal(1);
+		});
+
+		it('should convert completed false to 0', async function() {
+			await db.insert('daily', {
+				lastUpdated: new Date().toISOString(),
+				name: 'New Name',
+				completed: false
+			});
+
+			const result = await sqlite.get('SELECT completed FROM daily');
+			result.completed.should.eql(0);
+		});
+	});
+
+	describe('#del', function() {
+		var id;
+
+		beforeEach(loadEntries);
+		afterEach(clearEntries);
+
+		beforeEach(async function() {
+			db = new TasksDb();
+			await db.open({sqlite: DB_PATH});
+			id = (await sqlite.get('SELECT rowid FROM daily WHERE name = \'Test1\'')).rowid;
+		});
+
+		it('should return true if found', async function() {
+			const result = await db.del('daily', {id});
+			result.should.be.ok();
+		});
+
+		it('should return false if not found', async function() {
+			const result = await db.del('daily', {id: 'djkslj3lf'});
+			result.should.not.be.ok();
+		});
+
+		it('should remove row on success', async function() {
+			await db.del('daily', {id});
+			const result = await sqlite.get('SELECT * FROM daily WHERE name = \'Test 1\'');
+			should(result).not.be.ok();
+		});
+
+		it('should handle unusual criteria', async function() {
+			await db.del('daily', {completed: 0});
+
+			const completeCnt = testEntries.reduce((sum,x) => x.completed);
+			const result = await sqlite.run('SELECT COUNT(*) as cnt FROM daily');
+			result.changes.should.eql(completeCnt);
+		});
+	});
 });
